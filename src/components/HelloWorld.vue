915fec0e075f51ffe6c14f75f3d578f852744f2c
Georg Kothmeier <georg.kothmeier@roomle.com>
2020-12-14 12:12:20 +0100

    chore: initial example to get it running

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,37 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>
+    <div
+      ref="configurator"
+      style="width:800px;height:600px;"
+    >
+
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+<script lang="ts">
+  import RoomleSdk from "@roomle/web-sdk";
+
+  import {
+    onMounted,
+    ref,
+  } from 'vue';
+
+  export default {
+    setup() {
+      const configurator = ref(null);
+
+      onMounted(async () => {
+        const roomleConfigurator = await RoomleSdk.getConfigurator();
+        roomleConfigurator.boot();
+        RoomleSdk.setGlobalInitData({customApiUrl: 'https://api.roomle.com/v2'});
+        await roomleConfigurator.getApi().init(configurator.value);
+        await roomleConfigurator.getApi().loadConfigurableItemById("usm:frame");
+      });
+
+      return {
+        configurator,
+      };
+    },
+  };
 </script>
